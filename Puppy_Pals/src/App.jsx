32344f8d57a3209059947b23e4c188f897f3300b
Daniel Hassett <danielhassett@Daniels-Mac-mini.local>
2024-11-07 22:05:57 -0500

    my first commit

--- Puppy_Pals/src/App.jsx
+++ Puppy_Pals/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
-
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppyList)
   return (
-    <>
+
+      <div className="App"> 
+        { 
+   puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>
+   })
+  }
+</div>
       
-      <h1></h1>
-      <div> </div>
-      
-    </>
+    
   )
 }
 
