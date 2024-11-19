36ec6e27dff8aaed8b6207d8679bb1633914efb2
hassettd <hassettd@gmail.com>
2024-11-19 15:05:52 -0500

    Add files via upload

--- Puppy_Pals/src/App.jsx
+++ Puppy_Pals/src/App.jsx
@@ -3,20 +3,43 @@ import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null); 
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  
+  function handleClick() {
+    
+  }
+  
+  
+  console.log(featuredPup)
+  
+
   return (
 
       <div className="App"> 
-        { 
-   puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>
-   })
-  }
+      {featPupId && (
+    <div>
+      <h2>{featuredPup.name}</h2>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+    </div>
+  )}
+        {puppies.map((puppy) => {
+     return (
+      <div onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+      {puppy.name}
+      </div>
+      
+     );
+   })}
+   
 </div>
       
     
-  )
+  );
 }
 
 export default App
